546003f8364efb09f38663cb477d14dce07113c3
Jordan Hall <oighty6@gmail.com>
2025-07-23 15:31:30 -0500

    chore: remove set_m_mint ix

--- programs/ext_earn/src/instructions/admin/mod.rs
+++ programs/ext_earn/src/instructions/admin/mod.rs
@@ -6,7 +6,6 @@ pub mod initialize;
 pub mod remove_earn_manager;
 pub mod remove_wrap_authority;
 pub mod set_earn_authority;
-pub mod set_m_mint;
 
 pub use add_earn_manager::AddEarnManager;
 pub(crate) use add_earn_manager::__client_accounts_add_earn_manager;
@@ -20,8 +19,6 @@ pub use remove_wrap_authority::RemoveWrapAuthority;
 pub(crate) use remove_wrap_authority::__client_accounts_remove_wrap_authority;
 pub use set_earn_authority::SetEarnAuthority;
 pub(crate) use set_earn_authority::__client_accounts_set_earn_authority;
-pub use set_m_mint::SetMMint;
-pub(crate) use set_m_mint::__client_accounts_set_m_mint;
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "cpi")] {
@@ -29,7 +26,6 @@ cfg_if::cfg_if! {
         pub(crate) use initialize::__cpi_client_accounts_initialize;
         pub(crate) use remove_earn_manager::__cpi_client_accounts_remove_earn_manager;
         pub(crate) use set_earn_authority::__cpi_client_accounts_set_earn_authority;
-        pub(crate) use set_m_mint::__cpi_client_accounts_set_m_mint;
         pub(crate) use add_wrap_authority::__cpi_client_accounts_add_wrap_authority;
         pub(crate) use remove_wrap_authority::__cpi_client_accounts_remove_wrap_authority;
     }

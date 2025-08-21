93c47229dba8a43b44ddaadd6cebd9f5bbbcd799
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-08-21 14:23:51 -0400

    simplify

--- services/repl/source/app.tsx
+++ services/repl/source/app.tsx
@@ -1,50 +1,40 @@
 import React, { useState } from 'react';
-import { Text, Box, useInput } from 'ink';
+import { Text, Box } from 'ink';
 import Swap from './swap.js';
-
-const ACTIONS = ['Swap', 'SwapAndBridge'];
+import { Select } from '@inkjs/ui';
+import SwapAndBridge from './swap-and-bridge.js';
 
 export type BaseProps = {
   network: string | undefined;
 };
 
 export default function App({ network = 'mainnet' }: BaseProps) {
-  const [selectedAction, setSelectedAction] = useState(0);
-  const [confirmedAction, setConfirmedAction] = useState<number | undefined>();
+  const [selectedAction, setSelectedAction] = useState<undefined | string>();
 
-  useInput((input, key) => {
-    if (key.upArrow) {
-      setSelectedAction((prev) => (prev - 1 + ACTIONS.length) % ACTIONS.length);
-    } else if (key.downArrow) {
-      setSelectedAction((prev) => (prev + 1) % ACTIONS.length);
-    } else if (key.return) {
-      setConfirmedAction(selectedAction);
-    }
-  });
-
-  if (!confirmedAction)
+  if (!selectedAction)
     return (
       <Box flexDirection="column" aria-role="list">
         <Text>Select an action:</Text>
-        {ACTIONS.map((item, index) => {
-          const isSelected = index === selectedAction;
-          const label = isSelected ? `> ${item}` : `  ${item}`;
-
-          return (
-            <Box key={item} aria-role="listitem" aria-state={{ selected: isSelected }}>
-              <Text color={isSelected ? 'blue' : undefined}>{label}</Text>
-            </Box>
-          );
-        })}
+        <Select
+          options={[
+            {
+              label: 'Swap',
+              value: 'swap',
+            },
+            {
+              label: 'Swap and Bridge',
+              value: 'swapAndBridge',
+            },
+          ]}
+          onChange={setSelectedAction}
+        />
       </Box>
     );
 
-  switch (ACTIONS[confirmedAction]) {
-    case 'Swap':
+  switch (selectedAction) {
+    case 'swap':
       return <Swap network={network} />;
-    case 'SwapAndBridge':
-      return <Text>Swap and Bridge Component</Text>;
-    default:
-      return <Text>Unknown Action</Text>;
+    case 'swapAndBridge':
+      return <SwapAndBridge />;
   }
 }

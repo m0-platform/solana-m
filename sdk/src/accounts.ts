762f5a19a9ae2a7f097de10782ca6fea5e5cf4a4
Kurtis Streutker <kurtisstreutker@gmail.com>
2025-10-07 09:27:27 -0400

    fix accounts

--- sdk/src/accounts.ts
+++ sdk/src/accounts.ts
@@ -1,6 +1,7 @@
 import { Connection, PublicKey } from '@solana/web3.js';
 import BN from 'bn.js';
-import { getProgram } from './idl';
+import { BorshAccountsCoder, Idl } from '@coral-xyz/anchor';
+import { IdlDefinedFields } from '@coral-xyz/anchor/dist/cjs/idl';
 
 export interface EarnManagerData {
   isActive: boolean;
@@ -14,6 +15,10 @@ export interface GlobalAccountData {
   admin: PublicKey;
   extMint: PublicKey;
   mMint: PublicKey;
+  variant: 'Crank' | 'ScaledUi' | 'NoYield';
+  wrapAuthorities: PublicKey[];
+
+  // crank fields
   index?: BN;
   timestamp?: BN;
   earnAuthority?: PublicKey;
@@ -31,14 +36,194 @@ export interface EarnerData {
 
 export async function loadGlobal(connection: Connection, program: PublicKey): Promise<GlobalAccountData> {
   const [globalAccount] = PublicKey.findProgramAddressSync([Buffer.from('global')], program);
-  const global = await getProgram(connection, program).account.extGlobalV2.fetch(globalAccount);
+
+  // global account will differ depending on the program features
+  const globalData = (await connection.getAccountInfo(globalAccount))!.data;
+
+  const yieldType = globalData[140];
+  const decoder = yieldVariantsDecoder(yieldType);
+  const global = decoder.decode('ExtGlobalV2', globalData);
 
   return {
     admin: global.admin,
-    extMint: global.extMint,
-    mMint: global.mMint,
-    index: global.yieldConfig.lastExtIndex,
-    timestamp: global.yieldConfig.timestamp,
-    earnAuthority: global.yieldConfig.earnAuthority,
+    extMint: global.ext_mint,
+    mMint: global.m_mint,
+    variant: Object.keys(global.yield_config.yield_variant)[0] as 'Crank' | 'ScaledUi' | 'NoYield',
+    wrapAuthorities: global.wrap_authorities,
+    index: global.yield_config.last_ext_index,
+    timestamp: global.yield_config.timestamp,
+    earnAuthority: global.yield_config.earn_authority,
+  };
+}
+
+function yieldVariantsDecoder(variant: number) {
+  return new BorshAccountsCoder(extensionIDL(variant));
+}
+
+function extensionIDL(variant: number): Idl {
+  if (variant >= 3) {
+    throw new Error('Invalid yield variant, must be 0, 1, or 2');
+  }
+
+  const yieldVariants: IdlDefinedFields[] = [
+    [
+      {
+        name: 'yield_variant',
+        type: {
+          defined: {
+            name: 'YieldVariant',
+          },
+        },
+      },
+    ],
+    [
+      {
+        name: 'yield_variant',
+        type: {
+          defined: {
+            name: 'YieldVariant',
+          },
+        },
+      },
+      {
+        name: 'fee_bps',
+        type: 'u64',
+      },
+      {
+        name: 'last_m_index',
+        type: 'u64',
+      },
+      {
+        name: 'last_ext_index',
+        type: 'u64',
+      },
+    ],
+    [
+      {
+        name: 'yield_variant',
+        type: {
+          defined: {
+            name: 'YieldVariant',
+          },
+        },
+      },
+      {
+        name: 'earn_authority',
+        type: 'pubkey',
+      },
+      {
+        name: 'last_m_index',
+        type: 'u64',
+      },
+      {
+        name: 'last_ext_index',
+        type: 'u64',
+      },
+      {
+        name: 'timestamp',
+        type: 'u64',
+      },
+    ],
+  ];
+
+  return {
+    address: '',
+    instructions: [],
+    metadata: {
+      name: '',
+      version: '',
+      spec: '',
+    },
+    accounts: [
+      {
+        name: 'ExtGlobalV2',
+        discriminator: [116, 209, 219, 83, 70, 143, 55, 127],
+      },
+    ],
+    types: [
+      {
+        name: 'ExtGlobalV2',
+        type: {
+          kind: 'struct',
+          fields: [
+            {
+              name: 'admin',
+              type: 'pubkey',
+            },
+            {
+              name: 'pending_admin',
+              type: {
+                option: 'pubkey',
+              },
+            },
+            {
+              name: 'ext_mint',
+              type: 'pubkey',
+            },
+            {
+              name: 'm_mint',
+              type: 'pubkey',
+            },
+            {
+              name: 'm_earn_global_account',
+              type: 'pubkey',
+            },
+            {
+              name: 'bump',
+              type: 'u8',
+            },
+            {
+              name: 'm_vault_bump',
+              type: 'u8',
+            },
+            {
+              name: 'ext_mint_authority_bump',
+              type: 'u8',
+            },
+            {
+              name: 'yield_config',
+              type: {
+                defined: {
+                  name: 'YieldConfig',
+                },
+              },
+            },
+            {
+              name: 'wrap_authorities',
+              type: {
+                vec: 'pubkey',
+              },
+            },
+          ],
+        },
+      },
+      {
+        name: 'YieldConfig',
+        type: {
+          kind: 'struct',
+          fields: yieldVariants[variant],
+        },
+      },
+      {
+        name: 'YieldVariant',
+        repr: {
+          kind: 'rust',
+        },
+        type: {
+          kind: 'enum',
+          variants: [
+            {
+              name: 'NoYield',
+            },
+            {
+              name: 'ScaledUi',
+            },
+            {
+              name: 'Crank',
+            },
+          ],
+        },
+      },
+    ],
   };
 }
